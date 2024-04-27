5ca0ff46b935a6fb32008e29b7ff80b8c8b350cb
daghanerdonmez <daghanerdonmez@hotmail.com>
2024-04-28 00:09:51 +0300

    Update daghan

--- application/frontend/src/App.tsx
+++ application/frontend/src/App.tsx
@@ -1,24 +1,69 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Feed from "./components/Feed";
+import SearchBar from "./components/SearchBar";
 import { PostData } from "./interfaces/postInterface";
+import image1 from "./assets/dummyimages/image1.png";
+import image2 from "./assets/dummyimages/image2.png";
+import pp1 from "./assets/dummyimages/pp1.png";
+import pp2 from "./assets/dummyimages/pp2.png";
 
 function App() {
   const postsData: PostData[] = [
     {
       id: 1,
-      profilePic: "https://example.com/profilepic1.jpg",
-      username: "john_doe",
-      text: "This is the first sample post",
-      imageUrl: "https://example.com/sampleimage1.jpg",
-      likes: 10,
-      dislikes: 2,
-      commentsCount: 5,
+      profilePic: pp1,
+      username: "Can Öztemiz",
+      community: "Fenerbahçe",
+      communityLink: "fenerbahcelink",
+      text: "Sizce Fenerbahçe'nin Trabzonspor karşısındaki hücum hattı nasıl olmalı?",
+      imageUrl: image1,
+      likes: 278,
+      dislikes: 12,
+      commentsCount: 124,
+    },
+    {
+      id: 2,
+      profilePic: pp2,
+      username: "GalaGala123",
+      community: "Galatasaray",
+      communityLink: "galatasaraylink",
+      text: "Icardi'nin bugünkü performansı çok iyi değil miydi?",
+      imageUrl: image2,
+      likes: 543,
+      dislikes: 23,
+      commentsCount: 87,
     },
     {
       id: 2,
       profilePic: "https://example.com/profilepic2.jpg",
       username: "jane_doe",
+      community: "Fenerbahçe",
+      communityLink: "fenerbahcelink",
+      text: "This is the second sample post",
+      imageUrl: "https://example.com/sampleimage2.jpg",
+      likes: 15,
+      dislikes: 3,
+      commentsCount: 8,
+    },
+    {
+      id: 2,
+      profilePic: "https://example.com/profilepic2.jpg",
+      username: "jane_doe",
+      community: "Fenerbahçe",
+      communityLink: "fenerbahcelink",
+      text: "This is the second sample post",
+      imageUrl: "https://example.com/sampleimage2.jpg",
+      likes: 15,
+      dislikes: 3,
+      commentsCount: 8,
+    },
+    {
+      id: 2,
+      profilePic: "https://example.com/profilepic2.jpg",
+      username: "jane_doe",
+      community: "Fenerbahçe",
+      communityLink: "fenerbahcelink",
       text: "This is the second sample post",
       imageUrl: "https://example.com/sampleimage2.jpg",
       likes: 15,
@@ -30,7 +75,11 @@ function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <div className="dummydiv">
+        <SearchBar />
+      </div>
       <Feed posts={postsData}></Feed>
+      <div className="dummydiv"></div>
     </div>
   );
 }
