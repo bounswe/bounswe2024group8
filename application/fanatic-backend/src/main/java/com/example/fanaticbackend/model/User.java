db66f536c3a4deffddd189cfe8c3a4090cae255f
yektaercul <yektaercul@gmail.com>
2024-04-28 17:05:37 +0300

    added lombok anots and UserDetails init for auth

--- application/fanatic-backend/src/main/java/com/example/fanaticbackend/model/User.java
+++ application/fanatic-backend/src/main/java/com/example/fanaticbackend/model/User.java
@@ -1,44 +1,62 @@
 package com.example.fanaticbackend.model;
 
+import com.example.fanaticbackend.model.enums.Role;
 import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    Long id;
 
     @Column(nullable = false, unique = true)
-    private String username;
+    String username;
 
     @Column(nullable = false)
-    private String password;
+    String password;
 
-    // Getters and setters
-    public Long getId() {
-        return id;
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    public String getUsername() {
-        return username;
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
