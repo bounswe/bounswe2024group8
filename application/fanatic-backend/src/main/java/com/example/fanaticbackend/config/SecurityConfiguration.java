ebe833d5047bf10b3dc803d2ce54f7b659df72ea
yektaercul <yektaercul@gmail.com>
2024-04-30 10:11:59 +0300

    bug fix

--- application/fanatic-backend/src/main/java/com/example/fanaticbackend/config/SecurityConfiguration.java
+++ application/fanatic-backend/src/main/java/com/example/fanaticbackend/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
