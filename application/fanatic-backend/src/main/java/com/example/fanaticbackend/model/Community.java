8cc5bba7ed0a4b59a45a640c7d9f9737d125a0f1
yektaercul <yektaercul@gmail.com>
2024-05-14 14:40:43 +0300

    Community model

---
+++ application/fanatic-backend/src/main/java/com/example/fanaticbackend/model/Community.java
@@ -0,0 +1,32 @@
+package com.example.fanaticbackend.model;
+
+import com.example.fanaticbackend.model.enums.Team;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "communities")
+public class Community {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String name;
+
+    @Column(nullable = false)
+    String description;
+
+    @Column(nullable = false)
+    Team team;
+
+
+}
