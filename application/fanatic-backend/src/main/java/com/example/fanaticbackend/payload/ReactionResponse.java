bc4c2ba50dacb117ab56eeff80eed197615f472c
Oğuz Kağnıcı <oguzkagnici2002@hotmail.com>
2024-05-15 20:54:17 +0300

    reaction response

--- application/fanatic-backend/src/main/java/com/example/fanaticbackend/payload/ReactionResponse.java
+++ application/fanatic-backend/src/main/java/com/example/fanaticbackend/payload/ReactionResponse.java
@@ -1,4 +1,21 @@
 package com.example.fanaticbackend.payload;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ReactionResponse {
+    Long postId;
+
+    Long commentId;
+
+    Integer likes;
+
+    Integer dislikes;
+
+    Boolean bookmarked;
 }
