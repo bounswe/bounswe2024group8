6fccb4c24bfc406366f5daa2b9a8e2a2a6892f3d
yektaercul <yektaercul@gmail.com>
2024-10-15 17:01:59 +0300

    backend init

---
+++ 3Design/backend/threedesign/src/main/java/boun/group8/threedesign/ThreedesignApplication.java
@@ -0,0 +1,13 @@
+package boun.group8.threedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreedesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreedesignApplication.class, args);
+    }
+
+}
