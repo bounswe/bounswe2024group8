7cea4e49445aac157c104c89bf1ec899b8e93d61
erendonmezfomo <eren.donmez@fomo.gs>
2024-10-19 03:34:24 +0300

    Eject expo + install dependencies

---
+++ 3Design/mobile/metro.config.js
@@ -0,0 +1,12 @@
+const {getDefaultConfig} = require('expo/metro-config');
+const {mergeConfig} = require("@react-native/metro-config");
+
+/**
+ * Metro configuration
+ * https://metrobundler.dev/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
