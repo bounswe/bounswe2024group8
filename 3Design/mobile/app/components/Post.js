704d60e2bd0316a81c006b94dd1f6fdaa8ff601d
erendonmezfomo <eren.donmez@fomo.gs>
2024-10-20 16:44:39 +0300

    Implement 3d model viewer and mock post screen

---
+++ 3Design/mobile/app/components/Post.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text} from "react-native";
+import OBJViewer from '../components/ObjectViewer';
+import objFile from '../assets/Hamburger.obj';
+
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+    };
+  }
+
+  render() {
+    const { title, content, disableScroll } = this.props;
+
+    return (
+      <View style={styles.postContainer}>
+        <View
+        >
+          <Text style={styles.postTitle}>{title}</Text>
+          <Text style={styles.postContent}>{content}</Text>
+        </View>
+        <View>
+          <OBJViewer objFilePath={objFile} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  postContainer: {
+    backgroundColor: '#fff',
+    padding: 15,
+    marginVertical: 8,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  postTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  postContent: {
+    fontSize: 14,
+    marginTop: 5,
+  },
+  modelView: {
+    width: '100%',
+    height: 300,
+    backgroundColor: 'white',
+  },
+});
+
+export default Post;
--- 3Design/mobile/app/screens/FeedScreen.js
+++ 3Design/mobile/app/screens/FeedScreen.js
@@ -1,15 +1,59 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
 import { Colors } from '../constants/Colors';
-import { AuthContext } from '../context/AuthContext';
+import Post from '../components/Post';
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+
+const mockPosts = [
+  {
+    id: '1',
+    title: 'First Post',
+    content: 'This is the content of the first post. It is very interesting!',
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'This is the content of the second post. More interesting stuff!',
+  },
+  {
+    id: '3',
+    title: 'Third Post',
+    content: 'This is the content of the third post. You will love this one!',
+  },
+  {
+    id: '4',
+    title: 'Fourth Post',
+    content: 'This is the content of the fourth post',
+  },
+];
 
 export default function FeedScreen() {
-  const { user } = useContext(AuthContext);
+  const flatListRef = useRef(null);
+
+  const disableScroll = (isDisabled) => {
+    if (flatListRef.current) {
+      flatListRef.current.setNativeProps({ scrollEnabled: !isDisabled });
+    }
+  };
+
+
   return (
     <View style={styles.body}>
-      <Text style={styles.tempText}>
-        Welcome Back {user.firstName} {user.lastName}
-      </Text>
+      <GestureHandlerRootView>
+        <FlatList
+          ref={flatListRef}
+          data={mockPosts}
+          keyExtractor={(item) => item.id}
+          removeClippedSubviews={false}
+          renderItem={({ item }) => (
+            <Post
+              title={item.title}
+              content={item.content}
+              disableScroll={disableScroll}
+            />
+          )}
+        />
+      </GestureHandlerRootView>
     </View>
   );
 }
@@ -19,11 +63,6 @@ const styles = StyleSheet.create({
     flex: 1,
     display: 'flex',
     backgroundColor: Colors.light,
-    alignItems: 'center',
     padding: '3%',
-  },
-  tempText: {
-    fontSize: 20,
-    fontWeight: '700',
   },
 });
