36deaccaa380c721e57f61147381f6e54bb69060
oguzkagnici <oguzkagnici2002@hotmail.com>
2024-11-05 15:47:35 +0300

    Create AnnotationRepository and Service.

---
+++ 3Design/backend/threedesign/src/main/java/boun/group8/threedesign/repository/AnnotationRepository.java
@@ -0,0 +1,7 @@
+package boun.group8.threedesign.repository;
+
+import boun.group8.threedesign.model.Annotation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnnotationRepository extends JpaRepository<Annotation, Long> {
+}
