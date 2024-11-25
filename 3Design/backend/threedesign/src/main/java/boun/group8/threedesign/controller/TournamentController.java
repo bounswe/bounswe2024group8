e5048ebd9f008ece57edca77c73ff8b55de47f10
yektaercul <yektaercul@gmail.com>
2024-11-25 21:28:15 +0300

    hot-fix: add manual start endpoint for tournaments

--- 3Design/backend/threedesign/src/main/java/boun/group8/threedesign/controller/TournamentController.java
+++ 3Design/backend/threedesign/src/main/java/boun/group8/threedesign/controller/TournamentController.java
@@ -7,10 +7,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,5 +41,11 @@ public class TournamentController {
         }
 
         return ResponseEntity.ok(tournament);
+    }
+
+    @PostMapping("/manual-start")
+    public ResponseEntity<String> startNewTournaments() {
+        tournamentService.startNewTournaments();
+        return ResponseEntity.ok("Tournaments started");
     }
 }
