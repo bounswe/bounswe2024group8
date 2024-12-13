b7127c2b8fe65e6f97174f72a265a3aa541a3fe8
Onur Ç. <onur.cerli@gmail.com>
2024-12-13 15:10:26 +0300

    Renamed Designs menu as Gallery in mobile app and set Gallery posts as default over Discussion posts

--- 3Design/mobile/app/screens/FeedScreen.js
+++ 3Design/mobile/app/screens/FeedScreen.js
@@ -12,9 +12,9 @@ import Post from '../components/Post';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import axios from 'axios';
-import { AuthContext } from "../context/AuthContext";
-import { Categories } from "../constants/Categories";
-import {useCategories} from "../context/CategoryContext";
+import { AuthContext } from '../context/AuthContext';
+import { Categories } from '../constants/Categories';
+import { useCategories } from '../context/CategoryContext';
 
 export default function FeedScreen() {
   const flatListRef = useRef(null);
@@ -26,7 +26,7 @@ export default function FeedScreen() {
 
   const [posts, setPosts] = useState([]);
   const [filteredPosts, setFilteredPosts] = useState([]);
-  const [showVisual, setShowVisual] = useState(false);
+  const [showVisual, setShowVisual] = useState(true);
   const [isFollowing, setIsFollowing] = useState(false);
   const { user } = useContext(AuthContext);
 
@@ -40,7 +40,9 @@ export default function FeedScreen() {
     }
   };
 
-  const categoryName = categories.find((cat) => category && cat.value === category.value)?.label || '3Design';
+  const categoryName =
+    categories.find((cat) => category && cat.value === category.value)?.label ||
+    '3Design';
 
   const fetchPosts = async () => {
     let fetchedPosts = [];
@@ -118,9 +120,14 @@ export default function FeedScreen() {
         },
       });
       setIsFollowing((prevState) => !prevState);
-      console.log(`Successfully ${isFollowing ? 'unfollowed' : 'followed'} category.`);
+      console.log(
+        `Successfully ${isFollowing ? 'unfollowed' : 'followed'} category.`
+      );
     } catch (error) {
-      console.error(`Failed to ${isFollowing ? 'unfollow' : 'follow'} category:`, error);
+      console.error(
+        `Failed to ${isFollowing ? 'unfollow' : 'follow'} category:`,
+        error
+      );
     }
   };
 
@@ -136,40 +143,40 @@ export default function FeedScreen() {
 
   // Fetch remaining time for the tournament
   // Fetch remaining time for the tournament
-    useEffect(() => {
-      if (category) {
-        const url = `${process.env.EXPO_PUBLIC_VITE_API_URL}/api/v1/tournaments/leaderboard/${category.value}`;
-        axios
-          .get(url, {
-            headers: {
-              Authorization: `Bearer ${user.accessToken}`,
-              'Content-Type': 'multipart/form-data',
-            },
-          })
-          .then((response) => {
-            const endTimeISO = response.data[0]?.tournament?.endTime;
+  useEffect(() => {
+    if (category) {
+      const url = `${process.env.EXPO_PUBLIC_VITE_API_URL}/api/v1/tournaments/leaderboard/${category.value}`;
+      axios
+        .get(url, {
+          headers: {
+            Authorization: `Bearer ${user.accessToken}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((response) => {
+          const endTimeISO = response.data[0]?.tournament?.endTime;
 
-            if (endTimeISO) {
-              const endTime = new Date(endTimeISO).getTime();
-              const now = Date.now();
-              const timeLeftInSeconds = Math.floor((endTime - now) / 1000);
+          if (endTimeISO) {
+            const endTime = new Date(endTimeISO).getTime();
+            const now = Date.now();
+            const timeLeftInSeconds = Math.floor((endTime - now) / 1000);
 
-              setRemainingTime(timeLeftInSeconds);
-              setShowTournamentBox(true); // Show the tournament box
-            } else {
-              //console.error('endTime not found in the response');
-              setShowTournamentBox(false); // Do not show the tournament box
-            }
-
-            setIsLoadingTime(false);
-          })
-          .catch((error) => {
-            //console.error('Failed to fetch remaining time', error.response?.data || error.message);
-            setIsLoadingTime(false);
+            setRemainingTime(timeLeftInSeconds);
+            setShowTournamentBox(true); // Show the tournament box
+          } else {
+            //console.error('endTime not found in the response');
             setShowTournamentBox(false); // Do not show the tournament box
-          });
-      }
-    }, [category]);
+          }
+
+          setIsLoadingTime(false);
+        })
+        .catch((error) => {
+          //console.error('Failed to fetch remaining time', error.response?.data || error.message);
+          setIsLoadingTime(false);
+          setShowTournamentBox(false); // Do not show the tournament box
+        });
+    }
+  }, [category]);
 
   // Timer countdown
   useEffect(() => {
@@ -200,56 +207,58 @@ export default function FeedScreen() {
 
   const handleLeaderboardPress = () => {
     navigation.navigate('Leaderboard', {
-      category: category
+      category: category,
     });
   };
 
   return (
-      <View style={styles.body}>
-        {/* Category Name */}
-        <Text style={styles.categoryName}>{categoryName}</Text>
+    <View style={styles.body}>
+      {/* Category Name */}
+      <Text style={styles.categoryName}>{categoryName}</Text>
 
-        {/* Follow/Unfollow Button */}
-        {category && (
-          <TouchableOpacity
-            style={styles.followButton}
-            onPress={handleFollowUnfollow}
-          >
-            <Text style={styles.followButtonText}>
-              {isFollowing ? 'Unfollow' : 'Follow'} Category
+      {/* Follow/Unfollow Button */}
+      {category && (
+        <TouchableOpacity
+          style={styles.followButton}
+          onPress={handleFollowUnfollow}
+        >
+          <Text style={styles.followButtonText}>
+            {isFollowing ? 'Unfollow' : 'Follow'} Category
+          </Text>
+        </TouchableOpacity>
+      )}
+
+      {/* Reset Category Button */}
+      {category && (
+        <TouchableOpacity
+          style={styles.resetButton}
+          onPress={handleResetCategory}
+        >
+          <Text style={styles.resetButtonText}>X</Text>
+        </TouchableOpacity>
+      )}
+
+      {/* Weekly Tournament Box */}
+      {category && showTournamentBox && (
+        <View style={styles.tournamentBox}>
+          <Text style={styles.tournamentTitle}>
+            Weekly Tournament for {category.label} Category
+          </Text>
+          {isLoadingTime ? (
+            <ActivityIndicator size='small' color={Colors.dark} />
+          ) : (
+            <Text style={styles.timerText}>
+              Time left: {formatTime(remainingTime)}
             </Text>
-          </TouchableOpacity>
-        )}
-
-        {/* Reset Category Button */}
-        {category && (
+          )}
           <TouchableOpacity
-            style={styles.resetButton}
-            onPress={handleResetCategory}
+            style={styles.leaderboardButton}
+            onPress={handleLeaderboardPress}
           >
-            <Text style={styles.resetButtonText}>X</Text>
+            <Text style={styles.leaderboardButtonText}>See Leaderboard</Text>
           </TouchableOpacity>
-        )}
-
-        {/* Weekly Tournament Box */}
-        {category && showTournamentBox && (
-          <View style={styles.tournamentBox}>
-            <Text style={styles.tournamentTitle}>Weekly Tournament for {category.label} Category</Text>
-            {isLoadingTime ? (
-              <ActivityIndicator size="small" color={Colors.dark} />
-            ) : (
-              <Text style={styles.timerText}>
-                Time left: {formatTime(remainingTime)}
-              </Text>
-            )}
-            <TouchableOpacity
-              style={styles.leaderboardButton}
-              onPress={handleLeaderboardPress}
-            >
-              <Text style={styles.leaderboardButtonText}>See Leaderboard</Text>
-            </TouchableOpacity>
-          </View>
-        )}
+        </View>
+      )}
 
       {/* Toggle Buttons */}
       <View style={styles.toggleContainer}>
@@ -257,7 +266,7 @@ export default function FeedScreen() {
           style={[styles.toggleButton, showVisual && styles.activeToggle]}
           onPress={() => setShowVisual(true)}
         >
-          <Text style={styles.toggleText}>Designs</Text>
+          <Text style={styles.toggleText}>Gallery</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={[styles.toggleButton, !showVisual && styles.activeToggle]}
@@ -274,9 +283,15 @@ export default function FeedScreen() {
           data={filteredPosts}
           keyExtractor={(item) => item.postId.toString()}
           removeClippedSubviews={false}
-          keyboardShouldPersistTaps="handled"
+          keyboardShouldPersistTaps='handled'
           ListEmptyComponent={
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
               <Text style={{ textAlign: 'center', color: Colors.grey }}>
                 Follow more categories to see more posts!
               </Text>
@@ -366,37 +381,37 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 14,
   },
- tournamentBox: {
-   marginTop: 10,
-   padding: 15,
-   borderWidth: 2,
-   borderColor: Colors.dark,
-   borderRadius: 8,
-   backgroundColor: Colors.lightGray,
-   alignItems: 'center',
- },
- tournamentTitle: {
-   fontSize: 18,
-   fontWeight: 'bold',
-   color: Colors.dark,
-   textAlign: 'center',
-   marginBottom: 8, // Space between the title and timer
- },
- timerText: {
-   fontSize: 16,
-   color: Colors.dark,
-   textAlign: 'center',
-   marginBottom: 12, // Space between the timer and button
- },
- leaderboardButton: {
-   backgroundColor: Colors.primary,
-   paddingVertical: 8,
-   paddingHorizontal: 12,
-   borderRadius: 5,
- },
- leaderboardButtonText: {
-   color: Colors.dark,
-   fontSize: 16,
-   fontWeight: '600',
- },
+  tournamentBox: {
+    marginTop: 10,
+    padding: 15,
+    borderWidth: 2,
+    borderColor: Colors.dark,
+    borderRadius: 8,
+    backgroundColor: Colors.lightGray,
+    alignItems: 'center',
+  },
+  tournamentTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: Colors.dark,
+    textAlign: 'center',
+    marginBottom: 8, // Space between the title and timer
+  },
+  timerText: {
+    fontSize: 16,
+    color: Colors.dark,
+    textAlign: 'center',
+    marginBottom: 12, // Space between the timer and button
+  },
+  leaderboardButton: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 5,
+  },
+  leaderboardButtonText: {
+    color: Colors.dark,
+    fontSize: 16,
+    fontWeight: '600',
+  },
 });
