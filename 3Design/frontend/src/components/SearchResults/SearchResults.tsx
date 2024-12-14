54135278fc93b28b5a6a26ef10f22ae8a2d9d456
Hüseyin Türker Erdem <turkerdem99@gmail.com>
2024-12-14 17:15:42 +0300

    user search is implemented
    Co-authored-by: Deniz <deniz.poyraz@std.bogazici.edu.tr>
    Co-authored-by: Ersel <ersel.canakcili@boun.edu.tr>

--- 3Design/frontend/src/components/SearchResults/SearchResults.tsx
+++ 3Design/frontend/src/components/SearchResults/SearchResults.tsx
@@ -8,21 +8,51 @@ import DiscussionPost from '../DiscussionPost/Clickable/DiscussionPost';
 import SideBar from '../SideBar/SideBar';
 import PageHeader from '../PageHeader/PageHeader';
 import axios from 'axios';
+import UserDisplayer from './UserDisplayer/UserDisplayer';
+
+interface UserResponse{
+    userId : number;
+    email : string;
+    nickName : string;
+    profilePictureUrl : string;
+    experience : number;
+    isFollowed : boolean;
+}
 
 const SearchResults = () => {
     const {query} = useParams();
     const [searchResults, setSearchResults] = useState<DPost[]>([]);
     const [searchLoading, setSearchLoading] = useState(true);
+    const [searchType, setSearchType] = useState(true);
+    const [userResults, setUserResults] = useState<UserResponse[]>([]);
 
     if (query == undefined || query == null){
         window.location.href = "/home";
     }
 
     useEffect(() => {
-        fetchPostData();
-    }, [])
+        fetchData();
+    }, [searchType])
 
-    const fetchPostData = async () => {
+    const fetchData = async () => {
+        setSearchLoading(true);
+        if (!searchType){
+            try{
+                const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/v1/users/search?keyword=${query}`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("jwt_token")}`
+                    }
+                });
+                setUserResults(res.data);
+            }
+            catch(e){
+    
+            }
+            finally{
+                setSearchLoading(false);
+                return;
+            }
+        }
         try{
             const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/v1/posts?param=${query}`, {
                 headers: {
@@ -40,6 +70,13 @@ const SearchResults = () => {
 
     }
 
+    const searchTypeHandler = (x: boolean) => {
+        if (searchType == x){
+            return;
+        }
+        setSearchType(x);
+    }
+
 
     return (
         <>
@@ -50,7 +87,11 @@ const SearchResults = () => {
                     <div className='flex flex-col gap-6'>
                         <div>
                             <p className='font-bold text-xl'>Results for: {query}</p>
-                        </div>    
+                        </div>
+                        <div className='flex gap-8 justify-start'> 
+                            <button className='btn btn-neutral' style={!searchType ? {background: "#ffffff", color: "black"} : {background: "#d0d0d0", color: "black"}} onClick={() => searchTypeHandler(true)}>Posts</button>
+                            <button className='btn btn-neutral' style={searchType ? {background: "#ffffff", color: "black"} : {background: "#d0d0d0", color: "black"}} onClick={() => searchTypeHandler(false)}>Users</button>
+                        </div>     
                     </div>
                     { searchLoading ? 
                     (
@@ -58,14 +99,24 @@ const SearchResults = () => {
                             <Skeleton active avatar paragraph={{ rows: 4 }} />
                         </div>
                     ) : 
+                    searchType ?
+                        (
                         searchResults.length == 0 ? 
-                        <p>No results for this query</p> :
+                        <p>No post results for this query</p> :
                         searchResults.map((item, index) => (
                             item.isVisualPost ?
                             <GalleryPost  key={`g_${item.isVisualPost}`} postData={item}/> 
                             :
                             <DiscussionPost  key={`d_${item.isVisualPost}`} postData={item} />
-                        )) 
+                        ))
+                        ) :
+                        (
+                            userResults.length == 0 ? 
+                            <p>No user results for this query</p> :
+                            userResults.map((item, index) => (
+                                <UserDisplayer data={item}/>
+                            ))
+                        )
                     }           
                 </div>
                 
